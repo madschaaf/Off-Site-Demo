ed03461585ad8c889d03085fbd05b3600072f310
madschaaf <madschaaf@ebay.com>
2026-02-12 23:20:33 -0700

    Deploy React app to GitHub Pages - 2026-02-12 23:20:33

--- src/app/pages/LandingPage.tsx
+++
@@ -1,7 +0,0 @@
-import { MainContent } from '../components/MainContent';
-
-export function LandingPage() {
-  return (
-    <MainContent backgroundImage="https://images.unsplash.com/photo-1713463374257-16790466d9af?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxtb2Rlcm4lMjB0ZWNobm9sb2d5JTIwYWJzdHJhY3QlMjBiYWNrZ3JvdW5kfGVufDF8fHx8MTc3MDczNDMxM3ww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral" />
-  );
-}
